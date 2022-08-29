6178ecc5b32734a3295f0791899b575a0cb7f66c
Coreen Huang <coreen.huang@gmail.com>
2022-08-29 00:59:58 -0400

    Implement Map - Lotide Assignment

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// assertEqual function
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKeyByValue function
+
+const findKeyByValue = function(object, value) {
+  let genre = object[key];
+  
+};
+
+// test codes
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
---
+++ map.js
@@ -0,0 +1,71 @@
+// Our map function will take in two arguments:
+
+// An array to map
+// A callback function
+
+// The map function will return a new array based on the results of the callback function.
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+// TESTING BELOW
+
+// compare array lengths
+
+const compareArrayLengths = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return 'Both arrays have the same length!';
+  }
+  return false;
+};
+
+// go through each element in an array and compare them
+
+const compareEachElement = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// eqArrays function
+
+const eqArrays = function(arr1, arr2) {
+  if (!compareArrayLengths(arr1, arr2)) {
+    return false;
+  }
+  if (!compareEachElement(arr1, arr2)) {
+    return false;
+  }
+  return true;
+};
+
+// assertArraysEqual function
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// testing codes
+
+const testArray = ['jingle', 'bells', 'batman', 'smells']
+
+assertArraysEqual(map(testArray, word => word[0]), ['j', 'b', 'b', 's']);
+assertArraysEqual(map(testArray, word => word + word), ['jinglejingle', 'bellsbells', 'batmanbatman', 'smellssmells']);
+assertArraysEqual(map(testArray, word => word.length), [6, 5, 6, 6]);
