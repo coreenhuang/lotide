b696234ce88ccf1b4f45a8b4e9ba33e7387ebabe
Coreen Huang <coreen.huang@gmail.com>
2022-08-29 14:52:10 -0400

    refactored assertEqual and head functions with export & import, created new files for tests codes - assertEqualTest and headTest

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // CHALLENGE BELOW
 
@@ -12,9 +6,4 @@ const head = function(anyArray) {
   return anyArray[0];
 }
 
-// TEST CASES
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([5]), 5);
+module.exports = head;
