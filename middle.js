0cf0ed5723a89aa3f3eec710d98ae66621e9e5d0
Coreen Huang <coreen.huang@gmail.com>
2022-08-29 15:59:08 -0400

    refactored middle function with modules, new test file middleTest.js

--- middle.js
+++ middle.js
@@ -1,45 +1,3 @@
-// compare array lengths
-
-const compareArrayLengths = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    return 'Both arrays have the same length!';
-  }
-  return false;
-};
-
-// go through each element in an array and compare them
-
-const compareEachElement = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// eqArrays function
-
-const eqArrays = function(arr1, arr2) {
-  if (!compareArrayLengths(arr1, arr2)) {
-    return false;
-  }
-  if (!compareEachElement(arr1, arr2)) {
-    return false;
-  }
-  return true;
-};
-
-// assertArraysEqual function
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 // middle function BELOW
 
 // array with one or two elements will return empty array
@@ -85,3 +43,4 @@ const middle = function(anyArray) {
   return makeOutputArray(anyArray);
 };
 
+module.exports = {checkForSufficientElements, checkEvenOrOdd, makeOutputArray, middle};
